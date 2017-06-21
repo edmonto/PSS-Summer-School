105a92b9352da3fce7f937e52af83b9d6be6ce66
Edi Ursus <eduard.g.dan@gmail.com>
2017-06-20 22:03:53 -0500

    Added week 0 assignment, not compelted.

---
+++ Module 1/a0_m1_2017/a0_m1_2017.R
@@ -0,0 +1,18 @@
+# A0 skeleton code
+
+### Program's Process ###
+# Set your working directory
+# Load data into workspace
+# List all objects in memory
+# Recover structure of dataset
+
+
+#################################
+#       YOUR CODE HERE          #
+#################################
+
+
+
+#################################
+
+
